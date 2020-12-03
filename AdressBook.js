8f99338b4a75a44d1d1a8dd5c3cb4eb85db1944e
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2020-12-03 14:43:20 +0530

    Adding:To Sort The Entries By City, State or Zip

--- AdressBook.js
+++ AdressBook.js
@@ -165,9 +165,27 @@ function countByCityOrState(countCityOrState, choice){
     }
 }
 
+function sortContactByCity_State_Zip(inputToSort){
+    if(inputToSort == 1){
+        contactArray.sort(function(a, b) { return a.city.localeCompare(b.city)});
+        for(let i = 0; i < contactArray.length; i++)
+        console.log(contactArray[i].toString(),"\n");
+    }
+    if(inputToSort == 2){
+        contactArray.sort(function(a, b) { return a.state.localeCompare(b.state)});
+        for(let i = 0; i < contactArray.length; i++)
+        console.log(contactArray[i].toString(),"\n");
+    }
+    if(inputToSort == 3){
+        contactArray.sort(function(a, b) { return parseInt(a.zip) - parseInt(b.zip)});
+        for(let i = 0; i < contactArray.length; i++)
+        console.log(contactArray[i].toString(),"\n");
+    }
+}
+
 let choice = 0;
 do{
-    console.log("Press: \n1) Add Contact \n2) Edit Contact \n3) View Contact  \n4) Delete Contact \n5) Count Contacts \n6) Search City or State \n7) View Contact By city or state \n8) Count by city or state \n9) Sort by name \n0)Exit:");
+    console.log("Press: \n1) Add Contact \n2) Edit Contact \n3) View Contact  \n4) Delete Contact \n5) Count Contacts \n6) Search City or State \n7) View Contact By city or state \n8) Count by city or state \n9) Sort by name \n10) Sort By City, State or Zip \n0)Exit:");
     choice = Number(prompt("Enter your choice: "));
     if(choice == 1){
         addContact();
@@ -227,5 +245,10 @@ do{
     }
     if(choice == 9){
         console.log(contactArray.sort((a,b)=>a.firstName.localeCompare(b.firstName)));
+    }
+    if(choice == 10){
+        console.log("Sort Contacts based on \t1.) City \t2.) State \t3.) Zip")
+        let inputToSort = parseInt(prompt("Enter your choice:  "))
+        sortContactByCity_State_Zip(inputToSort);
     }
 }while(choice != 0);
