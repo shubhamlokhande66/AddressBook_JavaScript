012f87323cc1f65863e1991f86df3528427d0386
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2020-12-03 14:34:22 +0530

    Adding : To Get Number oF Contact Person

--- AdressBook.js
+++ AdressBook.js
@@ -155,9 +155,19 @@ function searchByCityOrState(searchCityOrState, choice){
     console.log("Contact: ",contacts);
 }
 
+function countByCityOrState(countCityOrState, choice){
+    let contacts = new Array();
+    if(choice == 1){
+        console.log("Contacts in "+countCityOrState+" city are: ",contactArray.filter(contact => contact.city == countCityOrState).reduce(contact => contact + 1, 0));
+    }
+    if(choice == 2){
+        console.log("Contacts in "+countCityOrState+" state are: ",contactArray.filter(contact => contact.state == countCityOrState).reduce(contact => contact + 1, 0));
+    }
+}
+
 let choice = 0;
 do{
-    console.log("Press: \n1) Add Contact \n2) Edit Contact \n3) View Contact  \n4) Delete Contact \n5) Count Contacts \n6) Search City or State \n7) View Contact By city or state \n0)Exit:");
+    console.log("Press: \n1) Add Contact \n2) Edit Contact \n3) View Contact  \n4) Delete Contact \n5) Count Contacts \n6) Search City or State \n7) View Contact By city or state \n8) Count by city or state \n0)Exit:");
     choice = Number(prompt("Enter your choice: "));
     if(choice == 1){
         addContact();
@@ -200,6 +210,18 @@ do{
                     break;
             case 2: let state = prompt("Enter the state name: ");
                     searchByCityOrState(state, 2);
+                    break;
+        }
+    }
+    if(choice == 8){
+        console.log("1) Count By City   2) Count By State");
+        let choice = Number(prompt("Enter your choice: "));
+        switch (choice){
+            case 1: let city = prompt("Enter the city name: ");
+                    countByCityOrState(city, 1);
+                    break;
+            case 2: let state = prompt("Enter the state name: ");
+                    countByCityOrState(state, 2);
                     break;
         }
     }
