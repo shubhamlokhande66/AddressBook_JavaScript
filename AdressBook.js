fd2e4c4f30bda51dc4521a17a7a69286070b0e4a
lokhandeshubham24 <lokhandeshubham66@gmail.com>
2020-12-03 13:32:30 +0530

    Adding: Vaild Contact Details

---
+++ AdressBook.js
@@ -0,0 +1,30 @@
+var prompt = require('prompt-sync')();
+class Contact {
+
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    toString() {
+        return "First name: " + this.firstName + "\nLast name: " + this.lastName + "\nAddress: " + this.address + "\nCity: " + this.city + "\nState: " + this.state + "\nZip: " + this.zip + "\nPhone number: " + this.phoneNumber + "\nEmail: " + this.email;
+    }
+}
+
+let contact = new Contact("Shubham","Lokhande","Shirdi","Ahmednagar","Maharashtra",413710,7083962843,"lokhandeshubham24@gmail.com");
+console.log(contact.toString());
